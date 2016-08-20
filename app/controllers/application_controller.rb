453ef0138ab3b449f7563f792175885438fce5a5
M A Manazir Ahsan <manazirahsan.developer@gmail.com>
2016-08-20 16:55:28 +0800

    Add greeting page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def greeting
+  	render text: "<html><center><h1>Assalamu alaikum</h1>Call me Manna</center><html>"
+  end
 end
